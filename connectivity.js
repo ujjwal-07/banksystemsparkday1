73d38f0e32c7d6cb85ee182076048599c6b6d2c8
ujjwal-07 <p.ujjwal.8888@gmail.com>
2022-01-22 00:48:35 +0530

    second update

--- connectivity.js
+++ connectivity.js
@@ -138,6 +138,6 @@ app.get("/getDataForMobileNature",  (req,res)=>{
         })
     })
 })
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 31173;
 
 app.listen(port, console.log(`Listening on port ${port}..`));
