eaa19ab09c9cd2be1b4645bd085316a71a62ea6b
ujjwal-07 <p.ujjwal.8888@gmail.com>
2022-04-30 23:04:45 +0530

    first commit

--- connectivity.js
+++ connectivity.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const multer = require('multer');
 var path = require('path');
-const { strictEqual } = require('assert');
-
+let alert = require("alert")
+var bodyParser = require('body-parser');
+var balanceOfUSer = 0;
+var setbalance = 0;
+var emaill = ''
+var toemail = ''
+var historyy = ''
+var n =''
 
 app.set("view engine", "ejs");
 
@@ -16,123 +21,186 @@ app.use(express.static('public'));
 
 var router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var mongoDB = 'mongodb://127.0.0.1/wallpaperdb';
+let bal = 0
+var mongoDB = 'mongodb://127.0.0.1/customerdb';
 mongoose.connect(mongoDB).then(()=>{
     console.log('db connected');
 }).catch(err=>{
     console.log(err)
 })
 
-const uploadWallpaperschema = new mongoose.Schema({
-device : String,
-wallpaper : String,
-image: String
+const uploadcustomerschema = new mongoose.Schema({
+name : String,
+email : String,
+balance: Number,
+history: String
 })
 
-const wallpapermodel = mongoose.model("model", uploadWallpaperschema)
+const uploaddata = mongoose.model("userData", uploadcustomerschema
+)
 
-const storage = multer.diskStorage({
-    destination:"./public/upload/",
+
+// GET API's
+
+
+app.get('/AddCustomer',(req,res)=>{
+    res.render("addCustomer");
+
+})
+
+
+app.get("/getCustomerData",  (req,res)=>{
+    uploaddata.find({}, (err,data)=>{
+        res.render('showUserData',{
+            dataList : data
+        })
+    })
+})
+
+
+
+app.get("/updatebalance/:email",  (req,res)=>{
+    let e = req.params.email;
+    e = e.substring(0)
+    uploaddata.find({email:e},(err,data)=>{
+        if(data){
+            balanceOfUSer = data[0].balance
+            res.render('update',{
+                dataList : data
+            })
+        
+    
+}
+    
+console.log(balanceOfUSer);
+
+})
+})
+
+
+
+app.get("/do/",(req,res)=>{
     
 
-    filename: function(request, file, callback){
-        callback(null, Date.now() + file.originalname);
-    },
+uploaddata.find({email:emaill},(err,user)=>{
+    if(user){
+        historyy = user[0].history;
+        historyy+= "Recieved "+setbalance+" from "+ toemail +" , \n";
+        // console.log(user[0].balance)
+        var myquery = { email: emaill };
+        var newvalues = { $set: {balance: parseInt(user[0].balance)   + parseInt(setbalance),history:historyy} };
+        uploaddata.updateOne(myquery,newvalues,(err,res)=>{
+    if(err) throw err;
+})
+        
+    
+        
+
+}
+res.redirect("http://localhost:8080/getCustomerData");
+
+
+
+})
 });
 
-const upload = multer({
-    storage: storage,
-    limits: {
-        fieldSize: 1024 * 1024 * 3,
-    },
+
+
+
+app.get("/update/:to/:from/:amt",  (req,res)=>{
+    setbalance = parseInt(req.params.amt);
+    emaill = req.params.to
+
+     
+e = req.params.from;
+uploaddata.find({email:e},(err,user)=>{
+    if(user){
+        toemail = user[0].email;
+        historyy = user[0].history;
+        historyy+= "Paid "+req.params.amt+" to "+ req.params.to +" , \n";
+        var j = (parseInt(user[0].balance)  -  parseInt(req.params.amt))
+        console.log(user)
+        console.log(user[0].balance)
+        var myquery = { email: e };
+        var newvalues = { $set: {balance: parseInt(user[0].balance)   -  parseInt(req.params.amt),history:historyy  } };
+        uploaddata.updateOne(myquery,newvalues,(err,res)=>{
+    if(err) throw err;
+
+})
+
+res.redirect("http://localhost:8080/do/");
+
+
+}
+
+
+})
+
+
+
+
+
+
+
+
+
 });
 
-app.get('/hii',(req,res)=>{
-    res.render("mean");
 
+
+app.get("/updatebalance",  (req,res)=>{
+    uploaddata.find({device : "Mobile"}, (err,data)=>{
+        res.render('updateBalance',{
+            dataList : data
+        })
+    })
 })
 
-app.post('/post', upload.single('file') ,(req, res)=>{
-    var imageFile = req.file.filename;
-    var success = req.file.fieldname+ "uploaded succesfully";
+app.get("/uu",  (req,res)=>{
+    uploaddata.find({}, (err,data)=>{
+        res.render('transfer',{
+            dataList : data
+        })
+    })
+})
 
-    var blog = new wallpapermodel({
-        device : req.body.device,
-        wallpaper : req.body.wallpaper,
-        image : imageFile
+
+// POST API's
+
+app.post('/post' ,(req, res)=>{
+
+
+    var blog = new uploaddata({
+        name : req.body.name,
+        email : req.body.email,
+        balance : req.body.balance
 
     });
-    blog.save((err,doc)=>{
-        if (err) throw err;
-    });
-    res.redirect("http://localhost:8080/hii");
-
-})
-
-
-app.get("/getData",  (req,res)=>{
-    wallpapermodel.find({device : "PC"}, (err,data)=>{
-        res.render('wallpaper',{
-            dataList : data
-        })
+    uploaddata.findOne({email:req.body.email},(err,user)=>{
+        if (user){
+            alert("This Email has been already registered")
+        }
+        else{
+        blog.save((err,doc)=>{
+            if (err) throw err;
+        });
+        res.redirect("http://localhost:8080/getCustomerData");
+    }
+    
     })
+  
+
 })
 
 
-app.get("/getDataForDark",  (req,res)=>{
-    wallpapermodel.find({wallpaper : "Dark Wallpaper"}, (err,data)=>{
-        res.render('wallpaper',{
-            dataList : data
-        })
-    })
+
+app.post("/updatebalanceamount/:bal",(req,res)=>{
+    res.send(req.params.bal);
 })
 
-app.get("/getDataForSports",  (req,res)=>{
-    wallpapermodel.find({wallpaper : "Sports"}, (err,data)=>{
-        res.render('wallpaper',{
-            dataList : data
-        })
-    })
-})
-
-app.get("/getDataForNature",  (req,res)=>{
-    wallpapermodel.find({wallpaper : "Nature"}, (err,data)=>{
-        res.render('wallpaper',{
-            dataList : data
-        })
-    })
-})
-
-app.get("/getDataForMobile",  (req,res)=>{
-    wallpapermodel.find({device : "Mobile"}, (err,data)=>{
-        res.render('wallpaperMobile',{
-            dataList : data
-        })
-    })
-})
-
-app.get("/getDataForMobileDark",  (req,res)=>{
-    wallpapermodel.find({ $and: [{device:"Mobile"},{wallpaper : "Dark Wallpaper"}]}, (err,data)=>{
-        res.render('wallpaperMobile',{
-            dataList : data
-        })
-    })
-})
-app.get("/getDataForMobileSports",  (req,res)=>{
-    wallpapermodel.find({ $and: [{device:"Mobile"},{wallpaper : "Sports"}]}, (err,data)=>{
-        res.render('wallpaperMobile',{
-            dataList : data
-        })
-    })
-})
-app.get("/getDataForMobileNature",  (req,res)=>{
-    wallpapermodel.find({ $and: [{device:"Mobile"},{wallpaper : "Nature"}]}, (err,data)=>{
-        res.render('wallpaperMobile',{
-            dataList : data
-        })
-    })
-})
 const port = process.env.PORT || 8080;
 
 app.listen(port, console.log(`Listening on port ${port}..`));
